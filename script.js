49c6d48b4ab500e2aa4eb76869d40093c804daa7
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2026-08-31 11:01:49 +0000

    hlk

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const botaoTema = document.getElementById('botao-tema');
 
+
+botaoTema.addEventListener('click', () => {
+
+    document.body.classList.toggle('tema-claro');
+    
+    if (document.body.classList.contains('tema-claro')) {
+        botaoTema.innerHTML = '<span>☀️</span> Modo Claro';
+    } else {
+        botaoTema.innerHTML = '<span>🌙</span> Modo Escuro';
+    }
+});
